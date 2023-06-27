e7d4e97358366c46b30005cc51a37308b10f71bc
matveyozi <106310421+matveyozi@users.noreply.github.com>
2023-06-27 14:39:48 +0000

    Deploying to gh-pages from @ matveyozi/goit-react-hw-08-phonebook@d9d324be5fbbe1eb5fae4c0267410967ad52ea30 🚀

---
+++ static/js/128.4d41e672.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[128],{9128:function(e,o,n){n.r(o),n.d(o,{default:function(){return h}});n(2791);var r=n(184),t=function(){return(0,r.jsxs)("h2",{children:["Welcom to the ",(0,r.jsx)("span",{children:"phonebook"})," from ",(0,r.jsx)("a",{target:"_blank",rel:"nofollow noopener noreferrer",href:"https://github.com/matveyozi",children:"matviyozi"})]})},h=function(){return(0,r.jsx)(t,{})}}}]);
+//# sourceMappingURL=128.4d41e672.chunk.js.map
